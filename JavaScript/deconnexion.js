cd81a2a6b30ed341c6e3e5953ce9bd44b153f880
massiva <massivaachibane.217@gmail.com>
2026-04-30 20:19:48 +0100

    Add files via upload

--- JavaScript/deconnexion.js
+++ JavaScript/deconnexion.js
@@ -1,18 +1,28 @@
+document.getElementById("logoutBtn").addEventListener("click", function(e){
+    e.preventDefault();
+    logout();
+});
 async function logout() {
     try {
         const res = await fetch("https://mknay.alwaysdata.net/php/deconnexion.php", {
             method: "POST",
-            credentials: "include"
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            }
         });
 
         const data = await res.json();
 
         if (data.success) {
-            // 🔥 send signal to navbar system
+
+            localStorage.removeItem("logged_in");
+            localStorage.removeItem("user_nom");
+            localStorage.removeItem("user_prenom");
+
             localStorage.setItem("auth_event", Date.now());
 
-            // redirect
-            window.location.href = "index.html";
+            window.location.href = "../index.html";
         }
 
     } catch (err) {
